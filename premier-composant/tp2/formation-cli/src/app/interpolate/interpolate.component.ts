268f016f56dbb09c5fee2a8e0c87eb0b525438c7
Sony LAURET <sony.lauret@gmail.com>
2018-06-08 15:39:18 +0200

    caché text

--- premier-composant/tp2/formation-cli/src/app/interpolate/interpolate.component.ts
+++ premier-composant/tp2/formation-cli/src/app/interpolate/interpolate.component.ts
@@ -11,6 +11,8 @@ export class InterpolateComponent implements OnInit {
   nom:string='Duchene';
   formation:Formation= new Formation('Angular',"apprentissage d'angular");
   formationJs:Formation;
+  
+
   constructor( ) {
   }
 
@@ -24,7 +26,7 @@ export class InterpolateComponent implements OnInit {
   ngOnInit() {
       setTimeout(() => {
         this.formationJs= new Formation('Module JavaScript',"c'est l'ancêtre de typescript.");
-        }, 3000)
+        }, 3000)        
   }
 
 }
